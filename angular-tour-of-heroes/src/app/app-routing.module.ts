4e764b03422689ef3fc8a2238702179cf72acf7d
Bui The Hoa <buithehoa@gmail.com>
2018-01-03 08:43:01 +0700

    RouterModule.forRoot().

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
 
-@NgModule({
-  exports: [ RouterModule ]
-})
-export class AppRoutingModule { }
-
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent }
 ];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
+})
+export class AppRoutingModule { }
