dc954e7761d9276b96351f90be8986a7efaeb0c3
Bui The Hoa <buithehoa@gmail.com>
2018-01-08 10:11:02 +0700

    Simulate a data server.

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -24,9 +26,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AppRoutingModule,
     BrowserModule,
     FormsModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
     HttpClientModule
   ],
-  providers: [ HeroService, MessageService ],
-  bootstrap: [AppComponent]
+  providers: [ HeroService, MessageService, InMemoryDataService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
